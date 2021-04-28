869490d8836c191312fffcce7d4264d9eb0202ef
tr3yd4n <80318628+tr3yd4n@users.noreply.github.com>
2021-04-28 15:41:24 +0100

    Add files via upload

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -2,13 +2,13 @@ import { Navbar, Nav } from 'react-bootstrap'
 
 const MyNavBar = () => (
     <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">React-Book-strap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-                <Nav.Link href="#link">Menu</Nav.Link>
-                <Nav.Link href="#link">Reservations</Nav.Link>
-                <Nav.Link href="#link">Contact Us</Nav.Link>
+                <Nav.Link href="#link">Books</Nav.Link>
+                <Nav.Link href="#link">More Books...</Nav.Link>
+                <Nav.Link href="#link">Even More Books?!</Nav.Link>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
--- src/components/WarningSign.jsx
+++ src/components/WarningSign.jsx
@@ -1,7 +1,7 @@
 import { Alert } from 'react-bootstrap'
 
 const WarningSign = () => (
-    <Alert variant="success">
+    <Alert variant="warning">
         <Alert.Heading>Hey, nice to see you</Alert.Heading>
         <p>
             Aww yeah, you successfully read this important alert message. This example
